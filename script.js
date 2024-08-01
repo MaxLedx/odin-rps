ce629ddc0c16c97354c34873d1d472ddc2a6f62f
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2024-08-01 20:09:22 +0200

    Add function to play a single round

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let humanScore = 0;
+let computerScore = 0;
 
-console.log(getHumanChoice());
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
 
 function getComputerChoice() {
     const randomNumber = Math.random();
@@ -14,9 +19,35 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     while (true) {
-        const input = prompt('rock, paper, scissors ?');
+        const input = prompt('rock, paper, scissors ?').toLowerCase();
         if (input === 'rock' || input === 'paper' || input === 'scissors') {
             return input;
         }
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    const lowerHumanChoice = humanChoice.toLowerCase();
+    const lowerComputerChoice = computerChoice.toLowerCase();
+    if (lowerHumanChoice === lowerComputerChoice) {
+        console.log(`Tie! ${lowerHumanChoice} equals ${lowerComputerChoice}`);
+    } else if (lowerHumanChoice === 'rock' && lowerComputerChoice === 'paper') {
+        console.log(`You lose! ${lowerComputerChoice} beats ${lowerHumanChoice}`);
+        computerScore++;
+    } else if (lowerHumanChoice === 'rock' && lowerComputerChoice === 'scissors') {
+        console.log(`You win! ${lowerHumanChoice} beats ${lowerComputerChoice}`);
+        humanScore++;
+    } else if (lowerHumanChoice === 'paper' && lowerComputerChoice === 'rock') {
+        console.log(`You win! ${lowerHumanChoice} beats ${lowerComputerChoice}`);
+        humanScore++;
+    } else if (lowerHumanChoice === 'paper' && lowerComputerChoice === 'scissors') {
+        console.log(`You lose! ${lowerComputerChoice} beats ${lowerHumanChoice}`);
+        computerScore++;
+    } else if (lowerHumanChoice === 'scissors' && lowerComputerChoice === 'rock') {
+        console.log(`You lose! ${lowerComputerChoice} beats ${lowerHumanChoice}`);
+        computerScore++;
+    } else if (lowerHumanChoice === 'scissors' && lowerComputerChoice === 'paper') {
+        console.log(`You win! ${lowerHumanChoice} beats ${lowerComputerChoice}`);
+        humanScore++;
     }
 }
