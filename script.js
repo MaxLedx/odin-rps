8518c8c7f9fbdc7c0130754f546fbc42f32840dd
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2025-01-14 20:16:10 +0100

    feat: add function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
+function getComputerChoice() {
+    const random = Math.random();
+    if (random < 1 / 3) {
+        return ROCK;
+    } else if (random < 1 / 2) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
