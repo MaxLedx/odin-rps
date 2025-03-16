8f0540205461e6bca56b902241b2ddc6c2e39c9c
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2025-03-16 12:02:56 +0100

    feat: add get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+
+console.log(getComputerChoice());
+
+function getComputerChoice() {
+    const random = Math.random();
+    if (random <= 1.0 / 3.0) {
+        return ROCK;
+    } else if (random <= 2.0 / 3.0) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
