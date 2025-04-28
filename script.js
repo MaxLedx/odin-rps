f7cf9d98140bcf5fb21ba2b98f9dea11771fe970
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2025-04-28 14:25:14 +0200

    feat: add playGame

--- script.js
+++ script.js
@@ -1,61 +1,87 @@
-const values = {
-    rock: 'rock',
-    paper: 'paper',
-    scissors: 'scissors',
-};
+playGame();
 
-const scores = {
-    humanScore: 0,
-    computerScore: 0,
-}
+function playGame() {
+    const values = {
+        rock: 'rock',
+        paper: 'paper',
+        scissors: 'scissors',
+    };
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+    const players = {
+        none: 'none',
+        human: 'human',
+        computer: 'computer',
+    };
 
-playRound(humanSelection, computerSelection);
+    const scores = {
+        humanScore: 0,
+        computerScore: 0,
+    };
 
-function getComputerChoice() {
-    const random = Math.floor(Math.random() * 3);
-    if (random == 0) {
-        return values.rock;
-    } else if (random == 1) {
-        return values.paper;
-    } else {
-        return values.scissors;
-    }
-}
+    const getHumanChoice = () => prompt('rock, paper or scissors ?');
 
-function getHumanChoice() {
-    return prompt('rock, paper or scissors ?');
-}
+    const sanitizeHumanChoice = humanChoice => humanChoice.toLowerCase();
 
-function playRound(humanChoice, computerChoice) {
-    const sanitizedHumanChoice = humanChoice.toLowerCase();
-    if (sanitizedHumanChoice === computerChoice) {
-        console.log('Tie!');
-    } else if (sanitizedHumanChoice === values.rock) {
-        if (computerChoice === values.paper) {
-            console.log(`You lose! ${values.paper} beats ${values.rock}`);
-            scores.computerScore++;
+    const getComputerChoice = () => {
+        const random = Math.floor(Math.random() * 3);
+        if (random === 0) {
+            return values.rock;
+        } else if (random === 1) {
+            return values.paper;
         } else {
-            console.log(`You win! ${values.rock} beats ${values.scissors}`);
-            scores.humanScore++;
-        }
-    } else if (sanitizedHumanChoice === values.paper) {
-        if (computerChoice === values.rock) {
-            console.log(`You win! ${values.paper} beats ${values.rock}`);
-            scores.humanScore++;
-        } else {
-            console.log(`You lose! ${values.scissors} beats ${values.paper}`);
-            scores.computerScore++;
-        }
-    } else {
-        if (computerChoice === values.rock) {
-            console.log(`You lose! ${values.rock} beats ${values.scissors}`);
-            scores.computerScore++;
-        } else {
-            console.log(`You win! ${values.scissors} beats ${values.paper}`);
-            scores.humanScore++;
+            return values.scissors;
         }
     }
+
+    const logRoundResults = (winner, humanSelection, computerSelection) => {
+        if (winner === players.none) {
+            console.log('Round result: tie!');
+        } else if (winner === players.human) {
+            console.log(`Round result: you win! ${humanSelection} beats ${computerSelection}`);
+        } else {
+            console.log(`Round result: you lose! ${computerSelection} beats ${humanSelection}`);
+        }
+    };
+
+    const updateScores = winner => {
+        if (winner === players.human) {
+            scores.humanScore++;
+        }
+        if (winner === players.computer) {
+            scores.computerScore++;
+        }
+    }
+
+    const playRound = (humanChoice, computerChoice) => {
+        if (humanChoice === computerChoice) {
+            return players.none;
+        } else if (humanChoice === values.rock) {
+            return computerChoice === values.paper ? players.computer : players.human;
+        } else if (humanChoice === values.paper) {
+            return computerChoice === values.rock ? players.human : players.computer;
+        } else {
+            return computerChoice === values.rock ? players.computer : players.human;
+        }
+    }
+
+    const logGameResults = () => {
+        if (scores.humanScore === scores.computerScore) {
+            console.log('Game result: tie!');
+        } else if (scores.humanScore > scores.computerScore) {
+            console.log(`Game result: you win! ${scores.humanScore} to ${scores.computerScore}`);
+        } else {
+            console.log(`Game result: you lose! ${scores.humanScore} to ${scores.computerScore}`);
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const sanitizedHumanSelection = sanitizeHumanChoice(humanSelection);
+        const computerSelection = getComputerChoice();
+        const winner = playRound(sanitizedHumanSelection, computerSelection);
+        updateScores(winner);
+        logRoundResults(winner, sanitizedHumanSelection, computerSelection);
+    }
+
+    logGameResults();
 }
