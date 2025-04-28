0f9fbf9445b0ff486d746c20dfd84d23ecdaad8b
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2025-04-28 13:59:11 +0200

    feat: add playRound

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ const values = {
     scissors: 'scissors',
 };
 
-console.log(getHumanChoice());
+const scores = {
+    humanScore: 0,
+    computerScore: 0,
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 
 function getComputerChoice() {
     const random = Math.floor(Math.random() * 3);
@@ -19,4 +27,35 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     return prompt('rock, paper or scissors ?');
+}
+
+function playRound(humanChoice, computerChoice) {
+    const sanitizedHumanChoice = humanChoice.toLowerCase();
+    if (sanitizedHumanChoice === computerChoice) {
+        console.log('Tie!');
+    } else if (sanitizedHumanChoice === values.rock) {
+        if (computerChoice === values.paper) {
+            console.log(`You lose! ${values.paper} beats ${values.rock}`);
+            scores.computerScore++;
+        } else {
+            console.log(`You win! ${values.rock} beats ${values.scissors}`);
+            scores.humanScore++;
+        }
+    } else if (sanitizedHumanChoice === values.paper) {
+        if (computerChoice === values.rock) {
+            console.log(`You win! ${values.paper} beats ${values.rock}`);
+            scores.humanScore++;
+        } else {
+            console.log(`You lose! ${values.scissors} beats ${values.paper}`);
+            scores.computerScore++;
+        }
+    } else {
+        if (computerChoice === values.rock) {
+            console.log(`You lose! ${values.rock} beats ${values.scissors}`);
+            scores.computerScore++;
+        } else {
+            console.log(`You win! ${values.scissors} beats ${values.paper}`);
+            scores.humanScore++;
+        }
+    }
 }
